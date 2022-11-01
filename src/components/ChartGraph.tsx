7520a68131f1858d0813dea73f2d80bf107263ba
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-11-01 08:24:10 +0300

    aa

--- src/components/ChartGraph.tsx
+++ src/components/ChartGraph.tsx
@@ -25,89 +25,50 @@ ChartJS.register(
 );
 
 
-const DynamicChart = (props: any) => {
-    let title0 = "";
-    title0 = props.title;
-
-
-
-
-
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const [chartData, setChartData] = useState({
-        labels,
-        datasets: [
-            {
-                label: '',
-                data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                backgroundColor:
-                    'rgb(9,1,2)',
-
-                borderColor:
-                    'rgb(9,1,2)',
-
-                borderWidth: 1
-            },
-        ],
-    });
-
-
-    const Chart = () => {
-        let yAxisData: any = [];
-        let xAxisData: any = [];
-
-        axios.get("https://query1.finance.yahoo.com/v7/finance/quote?symbols=ABT,AZO,AVB,AVY,AVGO,BKR,BLL,BAC,BBWI,BAX,BDX,BBY,BIO,BIIB,BLK,BK,BA,BKNG,BWA,BXP,IBM,IP,IPG,MMM,MO,MTB,MRO,MPC,MKTX,MAR,MMC,MLM,MAS,MA,MTCH,MKC,MCD,MCK,MDT,MRK,MET,MTD,MGM,MCHP,MU,MSFT,MAA,MRNA,MHK,MDLZ,MPWR,MNST,MCO,MS,MOS,MSI,MSCI,NDAQ,NTAP,NFLX,NWL,NEM,NWSA,NWS,NEE,NLSN,NKE,NI,NSC,NTRS,NOC,NLOK,NCLH,NRG,NUE,NVDA,NVR,NXPI,NOW,ORLY,OXY,ODFL,OMC,OKE,ORCL,OGN,OTIS,O,PEAK,PCAR,PKG,PH,PAYX,PAYC,PYPL,PENN,PNR,PBCT,PEP,PKI,PFE,PM,PSX,PNW,PXD,PNC,ROST,RCL,SCHW,STZ,SJM,SPGI,SBAC,SLB,STX,SEE,SRE,SHW,SPG,SWKS,SNA,SO,SWK,SBUX,STT,STE,SYK,SIVB,SYF,SNPS,SYY,T,TECH,TAP,TMUS,TROW,TTWO,TPR,TGT,TEL,TDY,TFX,TER,TSLA,TXN,TXT,TMO,TJX,TSCO,TT,TDG,TRV,TRMB,TFC,TWTR,TYL,TSN,UDR,ULTA,USB,UAA,UA,UNP,UAL,UNH,UPS,URI,UHS,VLO,VTR,VRSN,VRSK,VZ,VRTX,VFC,VIAC,VTRS,V,VNO,VMC,WRB,WAB,WMT,WBA,WM,WAT,WEC,WFC,WELL,WST,WDC,WU,WRK,WY,WHR,WMB,WLTW,WYNN,XRAY,XOM,XEL,XYL,YUM,ZBRA,ZBH,ZION,ZTS")
-            .then(res => {
-
-                for (const dataObj of res.data.quoteResponse.result) {
-                    yAxisData.push(parseInt(dataObj.regularMarketPrice));
-                    xAxisData.push(parseInt(dataObj.marketCap));
-
-                }
-                setChartData({
-                    labels: xAxisData,
-                    datasets: [{
-                        label: 'index value',
-                        data: yAxisData,
-                        backgroundColor:
-                            'rgba(0,0,0,0.2)',
-
-                        borderColor:
-                            'rgb(16,2,2)',
-
-                        borderWidth: 1
-                    }]
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            })
-
-    }
-        Chart();
-
-
-
-
-
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top' as const,
-            },
-            title: {
-                display: true,
-                text: title0,
-            },
+const data = {
+    labels: ["21:00", "20:00", "19:00", "18:00", "17:00", "16:00", "15:00", "14:00","13:00", "12:00", "11:00", "10:00", "9:00"],
+    datasets: [
+        {
+            label: "S&P 500",
+            data: [3896.25, 3890.25, 3880.25, 3876.25, 3860.60, 3855.50,3870.25, 3990.20, 3870.90, 3860.25, 3850.60, 3855.50,3850.50 ],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)"
         },
-    };
+        {
+            label: "DOW",
+            data: [3596.25, 3490.25, 3780.25, 3476.25, 3060.60, 3155.50,3270.25, 3390.20, 3670.90, 3160.25, 3350.60, 3455.50,3550.50 ],
+            fill: false,
+            borderColor: "#742774"
+        }
+    ]
+};
 
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: "lmao",
+        },
+        zoom: {
+            enabled: true,
+            mode: "xy"
+        },
+        pan: {
+            enabled: true,
+            mode: "xy"
+        },
+    },
+};
 
+export default function ChartGraph(props: any) {
     return (
         <div className="chartArea">
-            <Line options={options} data={chartData}/>
+            <Line options={options} data={data} />
         </div>
-    )
+    );
 }
-export default DynamicChart;
