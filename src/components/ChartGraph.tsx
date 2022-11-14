e9f78086b077f01bbab104830ab19233de041ec6
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-11-14 23:25:20 +0300

    recharts

--- src/components/ChartGraph.tsx
+++ src/components/ChartGraph.tsx
@@ -1,113 +1,113 @@
 import React, {useState, useEffect} from 'react'
-
 import axios from 'axios';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import {Line} from 'react-chartjs-2';
-import './ChartGraph.css';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
+import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
 
 
-const DynamicChart = (props: any) => {
-    let title0 = "";
-    title0 = props.title;
-
-
-
-
-
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const [chartData, setChartData] = useState({
-        labels,
-        datasets: [
-            {
-                label: '',
-                data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                backgroundColor:
-                    'rgb(9,1,2)',
-
-                borderColor:
-                    'rgb(9,1,2)',
-
-                borderWidth: 1
-            },
-        ],
-    });
-
-
-    const Chart = () => {
-        let yAxisData: any = [];
-        let xAxisData: any = [];
-
-        axios.get("https://query1.finance.yahoo.com/v7/finance/quote?symbols=ABT,AZO,AVB,AVY,AVGO,BKR,BLL,BAC,BBWI,BAX,BDX,BBY,BIO,BIIB,BLK,BK,BA,BKNG,BWA,BXP,IBM,IP,IPG,MMM,MO,MTB,MRO,MPC,MKTX,MAR,MMC,MLM,MAS,MA,MTCH,MKC,MCD,MCK,MDT,MRK,MET,MTD,MGM,MCHP,MU,MSFT,MAA,MRNA,MHK,MDLZ,MPWR,MNST,MCO,MS,MOS,MSI,MSCI,NDAQ,NTAP,NFLX,NWL,NEM,NWSA,NWS,NEE,NLSN,NKE,NI,NSC,NTRS,NOC,NLOK,NCLH,NRG,NUE,NVDA,NVR,NXPI,NOW,ORLY,OXY,ODFL,OMC,OKE,ORCL,OGN,OTIS,O,PEAK,PCAR,PKG,PH,PAYX,PAYC,PYPL,PENN,PNR,PBCT,PEP,PKI,PFE,PM,PSX,PNW,PXD,PNC,ROST,RCL,SCHW,STZ,SJM,SPGI,SBAC,SLB,STX,SEE,SRE,SHW,SPG,SWKS,SNA,SO,SWK,SBUX,STT,STE,SYK,SIVB,SYF,SNPS,SYY,T,TECH,TAP,TMUS,TROW,TTWO,TPR,TGT,TEL,TDY,TFX,TER,TSLA,TXN,TXT,TMO,TJX,TSCO,TT,TDG,TRV,TRMB,TFC,TWTR,TYL,TSN,UDR,ULTA,USB,UAA,UA,UNP,UAL,UNH,UPS,URI,UHS,VLO,VTR,VRSN,VRSK,VZ,VRTX,VFC,VIAC,VTRS,V,VNO,VMC,WRB,WAB,WMT,WBA,WM,WAT,WEC,WFC,WELL,WST,WDC,WU,WRK,WY,WHR,WMB,WLTW,WYNN,XRAY,XOM,XEL,XYL,YUM,ZBRA,ZBH,ZION,ZTS")
-            .then(res => {
-
-                for (const dataObj of res.data.quoteResponse.result) {
-                    yAxisData.push(parseInt(dataObj.regularMarketPrice));
-                    xAxisData.push(parseInt(dataObj.marketCap));
-
-                }
-                setChartData({
-                    labels: xAxisData,
-                    datasets: [{
-                        label: 'index value',
-                        data: yAxisData,
-                        backgroundColor:
-                            'rgba(0,0,0,0.2)',
-
-                        borderColor:
-                            'rgb(16,2,2)',
-
-                        borderWidth: 1
-                    }]
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            })
-
+const data = [
+    {
+        "name": "Sunday",
+        "uv": 4000,
+        "pv": 2400,
+        "amt": 2400
+    },
+    {
+        "name": "Saturday",
+        "uv": 3000,
+        "pv": 1398,
+        "amt": 2210
+    },
+    {
+        "name": "Friday",
+        "uv": 2000,
+        "pv": 9800,
+        "amt": 2290
+    },
+    {
+        "name": "Thursday",
+        "uv": 2780,
+        "pv": 3908,
+        "amt": 2000
+    },
+    {
+        "name": "Wed",
+        "uv": 1890,
+        "pv": 4800,
+        "amt": 2181
+    },
+    {
+        "name": "Tuesday",
+        "uv": 2390,
+        "pv": 3800,
+        "amt": 2500
+    },
+    {
+        "name": "Monday",
+        "uv": 3490,
+        "pv": 4300,
+        "amt": 2100
+    },
+    {
+        "name": "Sunday",
+        "uv": 4000,
+        "pv": 2400,
+        "amt": 2400
+    },
+    {
+        "name": "Saturday",
+        "uv": 3000,
+        "pv": 1398,
+        "amt": 2210
+    },
+    {
+        "name": "Friday",
+        "uv": 2000,
+        "pv": 9800,
+        "amt": 2290
+    },
+    {
+        "name": "Thursday",
+        "uv": 2780,
+        "pv": 3908,
+        "amt": 2000
+    },
+    {
+        "name": "Wed",
+        "uv": 1890,
+        "pv": 4800,
+        "amt": 2181
+    },
+    {
+        "name": "Tuesday",
+        "uv": 2390,
+        "pv": 3800,
+        "amt": 2500
+    },
+    {
+        "name": "Monday",
+        "uv": 3490,
+        "pv": 4300,
+        "amt": 2100
     }
-        Chart();
 
+]
 
-
-
-
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top' as const,
-            },
-            title: {
-                display: true,
-                text: title0,
-            },
-        },
-    };
+function ChartGraph(props: any) {
 
 
     return (
-        <div className="chartArea">
-            <Line options={options} data={chartData}/>
-        </div>
-    )
+        <LineChart width={1000} height={500} data={data}
+                   margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <Tooltip/>
+            <Legend/>
+            <Line type="monotone" dataKey="pv" stroke="#8884d8"/>
+        </LineChart>
+
+    );
 }
-export default DynamicChart;
+
+
+export default ChartGraph;
